f11afc99f2f104ad7d1f0872c5e62f27670223f3
leon morival <leon.morival@gmail.com>
2025-01-20 10:05:50 +0100

    setup ok

--- src/configs/databases/mysql.config.ts
+++ src/configs/databases/mysql.config.ts
@@ -1,10 +1,13 @@
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectMySQLDB = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "3306"),
-  username: process.env.DB_USER || "root",
+  username: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "maptitecoloc",
   synchronize: true, // Attention : activez uniquement en développement
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-import { connectMongooseDB } from "./configs/databases/mongoose.config";
 import { connectMySQLDB } from "./configs/databases/mysql.config";
 import app from "./app";
 
@@ -9,16 +8,17 @@ const PORT = process.env.PORT || 3000;
 
 Promise.all([
   connectMySQLDB.initialize(), // Connexion à MySQL
-  connectMongooseDB(),        // Connexion à MongoDB
-]).then(() => {
-  console.log("Connected to MySQL and MongoDB!");
+  // Connexion à MongoDB
+])
+  .then(() => {
+    console.log("Connected to MySQL!");
 
-  // Lancer le serveur
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    // Lancer le serveur
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to initialize databases:", error);
+    process.exit(1); // Arrêter le processus si une connexion échoue
   });
-})
-.catch((error) => {
-  console.error("Failed to initialize databases:", error);
-  process.exit(1); // Arrêter le processus si une connexion échoue
-});
